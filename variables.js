2f0b0dcda3c93755b0317b22576b8786c43152af
GorbunovaAnna <anyutachke@gmail.com>
2022-07-01 11:57:54 +0300

    added module

---
+++ variables.js
@@ -0,0 +1,26 @@
+export const arrWords = [
+  "школа",
+  "поезд",
+  "яблоко",
+  "пингвин",
+  "океан",
+  "метеорит",
+  "праздник",
+  "цветок",
+  "солнце",
+  "зоопарк",
+];
+
+export const arrImg = [
+  "./img/1.png",
+  "./img/2.png",
+  "./img/3.png",
+  "./img/4.png",
+];
+
+export const img = document.querySelector(".img");
+export const button = document.querySelector("button");
+export const input = document.querySelector("input");
+export const wordContainer = document.querySelector(".word-wrapper");
+// export const spans = Array.from(document.querySelectorAll("span"));
+
